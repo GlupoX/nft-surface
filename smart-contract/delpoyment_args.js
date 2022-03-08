5baa213a9321a71b30b4d0fe87e35721cee06623
GlupoX <mickeypower@gmail.com>
2022-03-08 13:51:46 +0000

    deployment args file

---
+++ smart-contract/delpoyment_args.js
@@ -0,0 +1,6 @@
+module.exports = [
+    "GlupoX",            // ERC721 token name 
+    "GLUPOX",            // ERC721 token symbol
+    "50000000000000000", // Base mint price in wei, for lazy-minting. Can later be modified. 
+    "495"                // Royalty basis points, 495 mean 4.95%. IMMUTABE - cannot later be modified!
+];
